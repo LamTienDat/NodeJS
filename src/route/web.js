f96b1a2276aaad3e3b43bac2968ac94626d4485d
dat <lamdat280202@gmail.com>
2023-02-04 16:52:20 +0700

    delete function

--- src/route/web.js
+++ src/route/web.js
@@ -1,18 +1,21 @@
-
 import express from "express";
 import homeController from "../controllers/homeController";
 
-
 let router = express.Router();
 let initWebRoutes = (app) => {
-    router.get("/", homeController.getHomePage)
-    router.get("/crud", homeController.getCRUD)
-    
-    router.post("/post-crud", homeController.getPost)
+  router.get("/", homeController.getHomePage);
+  router.get("/crud", homeController.getCRUD);
 
-    router.get("/get-crud", homeController.displayGetCRUD)
+  router.post("/post-crud", homeController.getPost);
 
+  router.get("/get-crud", homeController.displayGetCRUD);
 
-    return app.use("/", router);
-}
-module.exports = initWebRoutes
+  router.get("/edit-crud", homeController.getEditCRUD);
+
+  router.post("/put-crud", homeController.putCRUD);
+
+  router.get("/delete-crud", homeController.deleteCRUD);
+
+  return app.use("/", router);
+};
+module.exports = initWebRoutes;
